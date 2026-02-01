9d0c99d7ed8f2ba08fede1d3b76dc58f90b8d243
jossduff <jduff360@gmail.com>
2026-02-01 09:45:00 -0500

    scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
-pub mod config;
+mod config;
+
+pub use config::Config;
+
+pub struct Node {
+    config: Config,
+}
+
+impl Node {
+    pub fn new(config: Config) -> Self {
+        Self { config }
+    }
+
+    pub fn run(self) {
+        // connect to all other nodes, then send ready check
+        //let connections = make_connections(self.config);
+        println!("I'm running!");
+    }
+}
+
+// fn make_connections(config: Config) -> todo!() {
+//     todo!()
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use clap::Parser;
-use dht::config::Config;
+use dht::{Config, Node};
 
 fn main() {
     let config = Config::parse();
 
     println!("I am {}", config.name);
     println!("I will connect to {:?}", config.connections);
+
+    let node = Node::new(config);
+    node.run();
 }
